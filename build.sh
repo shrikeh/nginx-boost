36454612affdbc38155b2e5cc104a26bf57b5f9d
Barney Hanlon <bhanlon@inviqa.com>
2015-06-24 14:00:08 +0100

    Disable stuff

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
 function build_nginx() {
+  set -e;
   local BASE_DIR=~/nginx;
-  local CONTRIB_PATH=${BASE_DIR}/modules/contrib;
-  local RESTY_PATH=${BASE_DIR}/modules/openresty;
-
+  local MODULES_PATH=${BASE_DIR}/modules;
+  local CONTRIB_PATH=${MODULES_PATH}/contrib;
+  local RESTY_PATH=${MODULES_PATH}/openresty;
   local BUILD_PATH=${BASE_DIR}/objs;
   local CORE_DIR=${BASE_DIR}/core;
+
+  local NGINX_PROXY_PREFIX_PATH=/usr/local/nginx-proxy
   cd ${CORE_DIR};
   ./configure \
     --builddir=${BUILD_PATH} \
+    --prefix=${NGINX_PROXY_PREFIX_PATH} \
     --with-http_ssl_module \
     --with-http_gzip_static_module \
     --with-http_realip_module \
     --with-http_spdy_module \
+    --without-http_fastcgi_module \
+    --without-http_uwsgi_module \
+    --without-http_scgi_module \
+    --without-mail_pop3_module \
+    --without-mail_imap_module \
+    --without-mail_smtp_module \
     --add-module=${CONTRIB_PATH}/ngx_devel_kit \
     --add-module=${CONTRIB_PATH}/nginx-length-hiding-filter-module \
     --add-module=${CONTRIB_PATH}/array-var-nginx-module \
@@ -21,11 +31,12 @@ function build_nginx() {
     --add-module=${CONTRIB_PATH}/headers-more-nginx-module \
     --add-module=${CONTRIB_PATH}/ngx_http_geoip2_module \
     --add-module=${CONTRIB_PATH}/redis2-nginx-module \
-  ;
+    --add-module=${CONTRIB_PATH}/ngx_pagespeed \
+    ;
+    make;
+    sudo make install;
+    cd ${BASE_DIR};
 
-  make;
-  make install;
-  cd ${BASE_DIR};
 }
 
 build_nginx
